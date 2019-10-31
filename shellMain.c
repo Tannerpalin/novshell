ce17519daaeb90363828ddc9cc2b739a163b2cd8
Tannerpalin <tcpa227@g.uky.edu>
2019-10-31 13:38:15 -0400

    Ready for scanner implementation.

--- shellMain.c
+++ shellMain.c
@@ -1,21 +1,31 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include<sys/wait.h>
 #define clean() printf("\033[H\033[J")
 
-void shellGreet() {
+void shellGreet() { // Simply clears screen and displays authors.
     clean();
     printf("***************Novel Shell****************\n");
     printf("  Authors: Tanner Palin and Will Shapiro\n");
     printf("******************************************\n");
 }
 
-void scanInput() { // TODO: Scan user input in, do not need to parse yet.
-    printf("%s : %s\n", getenv("USER"), getenv("PWD"));
-    clean();
+void parseTokens() { // TODO: Parse the user input into Tokens.
+
+}
+
+void scanInput() {
+    char* user = getenv("USER");
+    char* directory = getenv("PWD");
+    printf("%s @ %s \n", user, directory); // Displays user and current directory
+    
+    // TODO: Scan user input in, do not need to parse yet (Another function for parsing?).
+    scanf("$%s", user);    
 }
 
 int main() {
-    // TODO: Do main function things.
+    // TODO: Initialize variables buffers, make function calls.
     shellGreet();
     scanInput();
     while(1) {}
