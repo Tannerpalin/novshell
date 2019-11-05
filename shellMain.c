70d75f7f35d86a1d5520e46a2305fb201445617f
willcshapiro <willcshapiro@gmail.com>
2019-11-05 11:15:36 -0500

    added fgets and input retrieval as well as changed the loop to repeat the active dir each time

--- shellMain.c
+++ shellMain.c
@@ -11,23 +11,52 @@ void shellGreet() { // Simply clears screen and displays authors.
     printf("******************************************\n");
 }
 
-void parseTokens() { // TODO: Parse the user input into Tokens.
+char * parseTokens(char *instructionString) {
+    printf("this is the instructionString %s", instructionString);
+    for(int i = 0; i > sizeof(instructionString); i++){
+        
+    }
+    
+    return instructionString; // TODO: Parse the user input into Tokens.
 
 }
-void anyText(){} //TODO: 
-void scanInput() {
+void anyText(){
+
+} //TODO: 
+
+void variableAssignment(){
+
+} //TODO: 
+
+char * scanInput() {
+    char *userInput;
+    userInput =  malloc(sizeof(userInput)*(100));
     char* user = getenv("USER");
     char* directory = getenv("PWD");
-    printf("%s @ %s \n", user, directory); // Displays user and current directory
+    printf("%s @ %s $ ", user, directory); // Displays user and current directory
     
     // TODO: Scan user input in, do not need to parse yet (Another function for parsing?).
-    scanf("$%s", user);    
+    fgets(userInput,100, stdin);
+    //printf("%ld",sizeof(userInput));    
+    //printf("%s\n", userInput);
+
+    
+    return userInput;
 }
 
 int main() {
+    int quit = 0;
     // TODO: Initialize variables buffers, make function calls.
     shellGreet();
-    scanInput();
-    while(1) {}
+    
+    while(!quit) {
+        char * inputToConsole = scanInput();
+        // printf("%s\n", inputToConsole);
+        if(strncmp(inputToConsole, "bye",3)==0){
+            quit = 1;
+        }
+        char * successOrFailure = parseTokens(inputToConsole);
+
+        }
     return 0;
 }
